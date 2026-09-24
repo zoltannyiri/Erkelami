825160a455613c75f19cf17c142633ea15616599
Nyiri Zoltán <zoltan.nyiri02@gmail.com>
2026-09-24 23:59:26 +0200

    add prisma db

---
+++ backend/prisma/migrations/20260924215601_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Page" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Page_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Page_slug_key" ON "Page"("slug");
---
+++ backend/src/lib/prisma.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export default prisma;
